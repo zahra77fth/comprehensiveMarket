cc4253551ee687249ba92b899d18f4aa8551b1eb
Afagh <afagh@Afaghs-MacBook-Pro.local>
2025-02-14 02:46:57 +0330

    feat: add admin

--- app/admin/products/[id]/page.tsx
+++ app/admin/products/[id]/page.tsx
@@ -4,27 +4,27 @@ import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 export const metadata: Metadata = {
-  title: 'Update Product',
+    title: 'Update Product',
 };
 
 const AdminProductUpdatePage = async (props: {
-  params: Promise<{
-    id: string;
-  }>;
+    params: Promise<{
+        id: string;
+    }>;
 }) => {
-  const { id } = await props.params;
+    const { id } = await props.params;
 
-  const product = await getProductById(id);
+    const product = await getProductById(id);
 
-  if (!product) return notFound();
+    if (!product) return notFound();
 
-  return (
-    <div className='space-y-8 max-w-5xl mx-auto'>
-      <h1 className='h2-bold'>ویرایش محصول</h1>
+    return (
+        <div className='space-y-8 max-w-5xl mx-auto'>
+            <h1 className='h2-bold'>ویرایش محصول</h1>
 
-      <ProductForm type='Update' product={product} productId={product.id} />
-    </div>
-  );
+            <ProductForm type='Update' product={product} productId={product.id} />
+        </div>
+    );
 };
 
 export default AdminProductUpdatePage;
